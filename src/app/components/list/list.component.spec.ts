07508d44aedb9c281dbf0ad872bdbb5f3e2e16de
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2018-05-26 18:07:47 -0500

    agregando pruebas desarrollo

--- src/app/components/list/list.component.spec.ts
+++ src/app/components/list/list.component.spec.ts
@@ -1,5 +1,71 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { By } from '@angular/platform-browser';
+
+import { DebugElement } from '@angular/core';
+
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { RoutingComponents, AppRoutingModule } from '../../app.routing';
+
+import { FilterPipe } from '../../pipe/filter.pipe';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { NgPipesModule } from 'ngx-pipes';
+
+import {APP_BASE_HREF} from '@angular/common';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatBadgeModule,
+  MatAutocompleteModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+} from '@angular/material';
+
+
+
+
 import { ListComponent } from './list.component';
 
 describe('ListComponent', () => {
@@ -8,8 +74,54 @@ describe('ListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListComponent ]
-    })
+      declarations: [ ListComponent, RoutingComponents, FilterPipe ],
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientModule,
+        RouterTestingModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgPipesModule,
+        MatBadgeModule,
+        MatAutocompleteModule,
+        MatBottomSheetModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatExpansionModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatListModule,
+        MatMenuModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatStepperModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatTreeModule
+        ],
+        providers: [{provide: APP_BASE_HREF, useValue : '/' }]
+      })
     .compileComponents();
   }));
 
