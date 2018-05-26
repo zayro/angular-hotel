11bf8a27431c86e4f2ebd5ccc11f93d36dd9779a
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2018-05-26 16:26:08 -0500

    se eliminad admin de las rutas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,13 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Interal components
 import { HomeComponent } from './components/home/home.component';
-import { AdminComponent } from './components/admin/admin.component';
 import { ListComponent } from './components/list/list.component';
 import { MenuComponent } from './components/menu/menu.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'admin', component: AdminComponent },
   { path: '**',  redirectTo: '', pathMatch: 'full', component: HomeComponent }
 ];
 
@@ -18,4 +16,4 @@ const appRoutes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
-export const RoutingComponents = [HomeComponent, AdminComponent, ListComponent, MenuComponent];
+export const RoutingComponents = [HomeComponent, ListComponent, MenuComponent];
