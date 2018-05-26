28b38ad97c47686b6aba7a6f3cde7549046d2137
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2018-05-26 17:00:40 -0500

    eliminando archivos innecesarios

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Interal components
 import { HomeComponent } from './components/home/home.component';
-import { AdminComponent } from './components/admin/admin.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'admin', component: AdminComponent },
   { path: '**',  redirectTo: '', pathMatch: 'full', component: HomeComponent }
 ];
 
@@ -16,4 +14,4 @@ const appRoutes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
-export const RoutingComponents = [HomeComponent, AdminComponent];
+export const RoutingComponents = [HomeComponent];
--- src/app/components/root/root.component.spec.ts
+++ src/app/components/root/root.component.spec.ts
@@ -1,4 +1,50 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Routes, RouterModule } from '@angular/router';
+import {DomSanitizer} from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+
+import { RoutingComponents, AppRoutingModule } from '../../app.routing';
+
+import {
+  MatBadgeModule,
+  MatAutocompleteModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+} from '@angular/material';
+
 
 import { RootComponent } from './root.component';
 
@@ -8,7 +54,44 @@ describe('RootComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RootComponent ]
+      declarations: [ RoutingComponents, RootComponent ],
+      imports: [  MatBadgeModule,
+        MatAutocompleteModule,
+        MatBottomSheetModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatExpansionModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatListModule,
+        MatMenuModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatStepperModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatTreeModule
+        ]
     })
     .compileComponents();
   }));
